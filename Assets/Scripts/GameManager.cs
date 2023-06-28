837ef44b6c8c2694e671a911bb4a49a295a7e903
William Goniprow <wtgoniprow@gmail.com>
2023-06-27 22:50:28 -0700

    make game manager singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _instance;
+    public Camera camera;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance is null)
+            {
+                Debug.Log("Game Manager is Null");
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +35,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void generateLand()
+    {
+
     }
 }
