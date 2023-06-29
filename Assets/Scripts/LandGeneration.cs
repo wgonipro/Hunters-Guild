f5e19474118547bbacc73fb760a102992a0bd105
William Goniprow <wtgoniprow@gmail.com>
2023-06-28 19:09:59 -0700

    add engine tooling and add prefab structure

--- Assets/Scripts/LandGeneration.cs
+++ Assets/Scripts/LandGeneration.cs
@@ -1,9 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 public class LandGeneration : MonoBehaviour
 {
+    public GameObject[] commonPrefabs;
+    public GameObject[] uncommonPrefabs;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +19,10 @@ public class LandGeneration : MonoBehaviour
     {
         
     }
+}
+
+[EditorTool("Land Tool", typeof(LandGeneration))]
+class LandTool : EditorTool
+{
+
 }
